19009858dfe59af5a296d0085cb32fc3eaf0a5d6
TylerBo12 <Tylershovel@gmail.com>
2022-12-08 13:16:27 -0500

    WHAT.

--- vite-project/Javascript/main.js
+++ vite-project/Javascript/main.js
@@ -4,7 +4,21 @@ import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
 import "../Javascript/menu.js";
-import javascriptLogo from "../JavaScript/javascript.svg";
+/* import javascriptLogo from "../JavaScript/javascript.svg";
 import { setupCounter } from "../JavaScript/counter.js";
+ 
+setupCounter(document.querySelector("#counter"));*/
 
-setupCounter(document.querySelector("#counter"));
+const DOMselectors = {
+  buttons: document.querySelector(".btn1"),
+};
+
+DOMselectors.buttons.addEventListener(`click`, function () {
+  if (document.body.classList.contains("btn1")) {
+    document.body.classList.add("btn2");
+    document.body.classList.remove("btn1");
+  } else (document.body.classList.contains("btn2")){
+    document.body.classList.add("btn1");
+    document.body.classList.remove("btn2");
+  };
+});
