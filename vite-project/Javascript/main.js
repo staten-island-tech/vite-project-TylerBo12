9a60ed42e1946c1ecb083494804603c3ba09ed0f
TylerBo12 <Tylershovel@gmail.com>
2022-12-15 13:15:38 -0500

    Why is color so hard

--- vite-project/Javascript/main.js
+++ vite-project/Javascript/main.js
@@ -14,13 +14,13 @@ const DOMselectors = {
 };
 
 DOMselectors.colorChange.addEventListener(`click`, function () {
-  if (document.body.classList.contains("hotPink")) {
-    document.body.classList.add("light-Blue");
-    document.body.classList.remove("hotPink");
+  if (document.body.classList.contains("defaultTheme")) {
+    document.body.classList.add("alternateTheme");
+    document.body.classList.remove("defaultTheme");
   } else {
-    document.body.classList.contains("light-Blue");
-    document.body.classList.add("hotPink");
-    document.body.classList.remove("light-Blue");
+    document.body.classList.contains("alternateTheme");
+    document.body.classList.add("defaultTheme");
+    document.body.classList.remove("alternateTheme");
   }
 });
 
@@ -46,17 +46,17 @@ const myFunctions = {
     ).forEach((character) => {
       DOMselectors.parent.insertAdjacentHTML(
         "beforeend",
-        `<div class="child"><p>${character.name}</p> <p ><img class="imgs"src= ${character.img}></p> <p>${character.price}</p></div>`
+        `<div class="child"><p>${character.name}</p> <p ><img class="imgs"src= ${character.img}></p> <p>$${character.price}</p></div>`
       );
     });
   }),
   filterAll: DOMselectors.allBtn.addEventListener("click", function () {
     roster.Row1.filter((character) =>
-      character.properties.includes(`Floaty`)
+      character.properties.includes(`All`)
     ).forEach((character) => {
       DOMselectors.parent.insertAdjacentHTML(
         "beforeend",
-        `<div class="child"><p>${character.name}</p> <p ><img class="imgs"src= ${character.img}></p> <p> ${character.price}</p></div>`
+        `<div class="child"><p>${character.name}</p> <p ><img class="imgs"src= ${character.img}></p> <p> $${character.price}</p></div>`
       );
     });
   }),
