14ab4165631cf115f87748947661b5bad083182a
TylerBo12 <Tylershovel@gmail.com>
2022-12-20 13:13:53 -0500

    The Bacco btn isn't working :(

--- vite-project/Javascript/main.js
+++ vite-project/Javascript/main.js
@@ -1,7 +1,7 @@
+import "./functions.js";
 import "../Styles/style.css";
 import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
-import "./functions.js";
+import "aos/dist/aos.css"; // You can also use <link> for styles=
 AOS.init();
 import "./array.js";
 
@@ -19,8 +19,8 @@ DOMselectors.colorChange.addEventListener(`click`, function () {
   if (document.body.classList.contains("defaultTheme")) {
     document.body.classList.add("alternateTheme");
     document.body.classList.remove("defaultTheme");
-  } else {
-    document.body.classList.contains("alternateTheme");
+  } else document.body.classList.contains("alternateTheme");
+  {
     document.body.classList.add("defaultTheme");
     document.body.classList.remove("alternateTheme");
   }
